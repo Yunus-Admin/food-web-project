b0cb3efbe6fffb0865d958864dd1f81f5883f2da
Yunus Seytnazarov <seytnazarov.yunus@gmail.com>
2023-01-04 19:20:21 +0600

    optimization class MenuCard

--- js/script.js
+++ js/script.js
@@ -143,12 +143,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
   //Class for cards
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 44;
       this.changeToUAH();
@@ -160,9 +161,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
     render() {
       const element = document.createElement("div");
+
+      if (this.classes.length === 0) {
+        this.element = "menu__item";
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach((className) => element.classList.add(className));
+      }
+
       element.innerHTML = `
-        <div class="menu__item">
-            <img src=${this.src} alt=${this.alt} />
+          <img src=${this.src} alt=${this.alt} />
             <h3 class="menu__item-subtitle">${this.title}</h3>
             <div class="menu__item-descr">
               ${this.descr}
@@ -172,7 +180,7 @@ document.addEventListener("DOMContentLoaded", () => {
               <div class="menu__item-cost">Цена:</div>
               <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
             </div>
-          </div>
+          
       `;
       this.parent.append(element);
     }
@@ -186,7 +194,8 @@ document.addEventListener("DOMContentLoaded", () => {
       "свежих овощей и фруктов. Продукт активных и здоровых людей. Это\n" +
       "абсолютно новый продукт с оптимальной ценой и высоким качеством!",
     8,
-    ".menu .container"
+    ".menu .container",
+    "menu__item"
   ).render();
 
   new MenuCard(
@@ -197,7 +206,8 @@ document.addEventListener("DOMContentLoaded", () => {
       "но и качественное исполнение блюд. Красная рыба, морепродукты,\n" +
       "фрукты - ресторанное меню без похода в ресторан!",
     12,
-    ".menu .container"
+    ".menu .container",
+    "menu__item"
   ).render();
 
   new MenuCard(
@@ -209,6 +219,7 @@ document.addEventListener("DOMContentLoaded", () => {
       "овса, кокоса или гречки, правильное количество белков за счет тофу\n" +
       "и импортных вегетарианских стейков.",
     14,
-    ".menu .container"
+    ".menu .container",
+    "menu__item"
   ).render();
 });
